b94b4ed6d2105f0499512eca44a25b4a7cb4e7f0
Christopher Groskopf <staringmonkey@gmail.com>
2013-03-07 13:27:40 -0600

    Switch markdown to rst.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='relay',
     version='0.0.1',
     description='Meta-magical SSH tunnels for remote developers behind firewalls.',
-    long_description=open('README').read(),
+    long_description=open('README.rst').read(),
     author='Christopher Groskopf',
     author_email='staringmonkey@gmail.com',
     url='http://blog.apps.npr.org/',
