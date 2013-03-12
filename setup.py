3ae5696f48b200faa81a99409913e462dd255eb5
Christopher Groskopf <staringmonkey@gmail.com>
2013-03-12 15:52:23 -0500

    Rev to 0.0.9.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='relay',
-    version='0.0.8',
+    version='0.0.9',
     description='Meta-magical SSH tunnels for remote developers behind firewalls.',
     long_description=open('README.rst').read(),
     author='Christopher Groskopf',
